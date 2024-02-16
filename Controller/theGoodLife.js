3e17b20bbbe2e971641750131096ff9c5db21eef
Ritik Jadhav <ritiksjadhav99@gmail.com>
2024-02-16 21:21:20 +0530

    Schema updated, hash the password, userId created

--- Controller/theGoodLife.js
+++ Controller/theGoodLife.js
@@ -1,34 +1,50 @@
-const userModel = require('../Model/goodLifeSchema');
+const { Counter, User } = require('../Model/goodLifeSchema');
+const bcrypt = require('bcryptjs');
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid'); // Generating unique userId by using UUIDs (Universally Unique Identifiers)
 
-exports.userRegisteration = async (req, res) => {
+exports.userRegisteration = async (req, res, next) => {
     try {
-        const userId = uuidv4();
+        const email = await User.findOne({ email: req.body.email });
+        if (email) {
+            const err = new Error();
+            err.status = 400;
+            err.message = 'User already exists with this email id';
+            next(err);
+        } else {
+            // Find the counter document and increment the sequence
+            const counter = await Counter.findByIdAndUpdate(
+                { _id: 'userId' },
+                { $inc: { seq: 1 } }, // Increment the sequence field by one
+                { new: true, upsert: true } // Returns the updated document
+            );
 
-        const userData = {
-            ...req.body,
-            userId: userId,
+            const userId = 'UI-' + String(counter.seq).padStart(4, '0');
+            req.body.userId = userId;
+
+            const genSalt = bcrypt.genSaltSync(10); // Generate the salt 
+            const hashPassword = bcrypt.hashSync(req.body.password, genSalt); // Hash the password with the generated salt
+
+            req.body.password = hashPassword;
+            console.log(req.body);
+
+            const newUser = await User.create(req.body);
+            res.send('User registered successfully');
         }
-
-        const newUser = new userModel(userData);
-
-        await newUser.save();
-
-        res.status(201).json({
-            status: 'success',
-            message: 'User registered successfully',
-            data: {
-                user: newUser,
-            }
-        });
     } catch (error) {
-        console.log(error);
+        if (error.name === 'ValidationError') {
+            res.status(404).json({
+                message: error.message,
+            })
+            console.log(error.name, 'ritik');
+        }
+        console.log(error.message);
     }
 }
 
-exports.invalidPath = (req, res) => {
-    res.status(404).json({
-        message: 'invalid path',
-    })
+exports.invalidPath = (req, res, next) => {
+    let error = new Error();
+    error.status = 400;
+    error.message = 'Invalid Route';
+    next(error);
 }
--- Model/goodLifeSchema.js
+++ Model/goodLifeSchema.js
@@ -11,6 +11,8 @@ const uri = 'mongodb://127.0.0.1:27017/lifecoaches';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const validator = require('../Utilities/validator');
+
 //check connection status
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -19,55 +21,70 @@ db.once('open', () => {
 })
 
 //Schema
+const counterSchema = new mongoose.Schema({
+    _id: String,
+    seq: Number,
+});
+
 const userSchema = new mongoose.Schema(
     {
         userId: {
             type: String,
             unique: true,
-        }
-    },
-    {
+        },
         name: {
             type: String,
-            required: [true, 'Required field'],
-            minlength: [3, 'Name must be at least 3 characters long'],
-            maxlength: [50, 'Name cannot exceed 50 characters'],
-        }
-    },
-    {
+            required: [true, 'Name is required'],
+            minLength: [3, 'Name must be at least 3 characters long'],
+            maxLength: [50, 'Name cannot exceed 50 characters'],
+        },
         password: {
             type: String,
-        }
-    },
-    {
+            required: [true, 'Password is required'],
+            minLength: [6, 'Password should have minimum 6 characters'],
+        },
         gender: {
             type: String,
-        }
-    },
-    {
+            enum: {
+                values: ['M', 'F', 'Other'],
+                message: 'Gender should be either  M or F or Other',
+            }
+        },
         dateOfBirth: {
             type: Date,
-        }
-    },
-    {
+            required: [true, 'Date of Birth is required'],
+        },
         email: {
             type: String,
-        }
-    },
-    {
+            required: [true, 'Email is required'],
+            unique: true,
+            validate: {
+                validator: validator.validateEmail,
+                message: 'Enter a valid email id'
+            }
+        },
         mobileNumber: {
             type: String,
+            required: [true, 'Mobile Number is required'],
+            validate: {
+                validator: validator.validateMobileNo,
+                message: 'Mobile Number should have 10 digits'
+            }
         }
     },
     {
         timeStamps: {
             createdAt: true,
             updatedAt: true,
-        },
+        }
     }
 );
 
 //Model
-const userModel = mongoose.model('User', userSchema);
+const Counter = mongoose.model('Counter', counterSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports = userModel;
+module.exports = {
+    Counter: Counter,
+    User: User
+};
