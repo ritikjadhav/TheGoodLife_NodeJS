0fc98461983c7d2b67353816ed6a82b616f2f4bc
Ritik Jadhav <ritiksjadhav99@gmail.com>
2024-02-19 09:23:07 +0530

    Helmet middleware for security

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const route = require('./Routes/routing');
 const bodyParser = require('body-parser');
 const errorLogger = require('./Utilities/errorLogger');
+const helmet = require('helmet');
 
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet); //helmet middleware is a set of 14 small middleware functions that help in setting up security-related HTTP headers with default values
 
 app.use('/', route);
 app.use(errorLogger);
